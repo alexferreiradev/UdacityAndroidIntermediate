db026c38ba612de3370532819218a5cd2db6531b
Alex R. Ferreira <arf92@live.com>
2016-10-18 22:12:07 -0200

    add correção de Activity extend ActionBar.

--- UdacityPortifolio/app/src/main/java/com/alex/udacityportiflio/main/MainActivity.java
+++ UdacityPortifolio/app/src/main/java/com/alex/udacityportiflio/main/MainActivity.java
@@ -1,12 +1,12 @@
 package com.alex.udacityportiflio.main;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 
 import com.alex.udacityportiflio.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
