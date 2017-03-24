7edf369707886566635bd33aae1949b74785d183
Alex R. Ferreira <arf92@live.com>
2017-03-23 21:12:19 -0300

    add details activity

---
+++ Popular_Movies_App/PopularMoviesApp/app/src/main/java/com/alex/popularmovies/app/ui/view/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.alex.popularmovies.app.ui.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alex.popularmovies.app.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
