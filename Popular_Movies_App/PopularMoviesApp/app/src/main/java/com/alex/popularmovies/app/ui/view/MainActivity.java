aec35c98d0456642edb13610668c034dc31d33d5
Alex R. Ferreira <arf92@live.com>
2017-03-23 21:12:00 -0300

    organizando activity em pacote ui/view

--- Popular_Movies_App/PopularMoviesApp/app/src/main/java/com/alex/popularmovies/app/ui/view/MainActivity.java
+++ Popular_Movies_App/PopularMoviesApp/app/src/main/java/com/alex/popularmovies/app/ui/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.alex.popularmovies.app;
+package com.alex.popularmovies.app.ui.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.alex.popularmovies.app.R;
 
 public class MainActivity extends AppCompatActivity {
 
