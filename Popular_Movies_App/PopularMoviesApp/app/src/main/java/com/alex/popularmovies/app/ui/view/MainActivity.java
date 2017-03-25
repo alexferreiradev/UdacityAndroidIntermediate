baa892873103fc9114a856f334b8c349bcdcb994
Alex R. Ferreira <arf92@live.com>
2017-03-25 19:57:40 -0300

    add layout

--- Popular_Movies_App/PopularMoviesApp/app/src/main/java/com/alex/popularmovies/app/ui/view/MainActivity.java
+++ Popular_Movies_App/PopularMoviesApp/app/src/main/java/com/alex/popularmovies/app/ui/view/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends BaseActivity<Movie, MoviesPresenter.View, Movi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPresenter = new MoviesPresenter(this, this, savedInstanceState);
     }
 
     @Override
