e7948d0a1cf990e2b61c8e4e9c0974f3520feacd
Alex Ferreira <arf92@live.com>
2018-07-01 21:08:22 -0300

    #77 msg: Adicao de final em classes Utils

--- projetos_finais/Popular_Movies_App 2/PopularMoviesApp2/app/src/main/java/com/alex/popularmovies/app/util/MovieImageUtil.java
+++ projetos_finais/Popular_Movies_App 2/PopularMoviesApp2/app/src/main/java/com/alex/popularmovies/app/util/MovieImageUtil.java
@@ -9,12 +9,13 @@ import com.squareup.picasso.Picasso;
  * Created by Alex on 10/12/2017.
  */
 
-public class MovieImageUtil {
+final public class MovieImageUtil {
 	public static final String IMAGE_LENGTH_W_185 = "w185";
 	public static final String IMAGE_LENGTH_W_92 = "w92";
 
 	private static final String MOVIE_DB_BASE_IMAGE_PATH = "http://image.tmdb.org/t/p/";
 
+	@SuppressWarnings("StringBufferReplaceableByString")
 	public static void setImageViewWithPicasso(ImageView imageView, Context context, Movie movie, String imageLength) {
 		String path = new StringBuilder(MOVIE_DB_BASE_IMAGE_PATH)
 				.append(imageLength).append("/")
