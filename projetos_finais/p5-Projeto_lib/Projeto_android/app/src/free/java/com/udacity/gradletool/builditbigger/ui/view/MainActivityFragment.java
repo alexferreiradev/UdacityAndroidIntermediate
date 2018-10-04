39f9db36fab4891e238a044823ff8467ed43fda5
Alex Rabelo Ferreira <arf92@live.com>
2018-10-04 08:52:12 -0300

    #118 Separacao de free flavor
    * commit separado devido ide nao reconhecer e fazer build de ambos flavors
    * set flavors em build
    * criacao de sourceset paid e free
    * criacao de fragments para cada flavor
    * refatoracao para utilizar endpoint service para realizar requisicao para o endpoint
    * remocao de dependencias nao utilizadas de paid flavor

--- projetos_finais/p5-Projeto_lib/Projeto_android/app/src/free/java/com/udacity/gradletool/builditbigger/ui/view/MainActivityFragment.java
+++ projetos_finais/p5-Projeto_lib/Projeto_android/app/src/free/java/com/udacity/gradletool/builditbigger/ui/view/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -25,6 +26,14 @@ public class MainActivityFragment extends Fragment {
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+		Button tellJokeBT = root.findViewById(R.id.btTellJoke);
+		tellJokeBT.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				tellJoke(v);
+			}
+		});
 
 		AdView mAdView = root.findViewById(R.id.adView);
 		AdRequest adRequest = new AdRequest.Builder()
